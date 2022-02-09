fa973e245fa4739030744e793bc8f4dbaf456e89
Sampada <sampadahakke102@gmail.com>
2022-02-09 18:39:11 +0530

    Adding Employee Attendace Problem

---
+++ EmployeeWageComputation1/EmployeeWageComputation1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeWage employee = new EmployeeWage();
+            employee.EmployeeAttendance();
+            Console.ReadLine();
+        }
+    }
+}
